7c0cef3863629ac6dbef98999642ba332215e636
Firenz <firenz.shizu@gmail.com>
2014-06-23 00:06:13 +0200

    modificado GDD y añadida memoria del proyecto final de carrera

---
+++ documentacion/memoria/codigo/hola_mundo.cpp
@@ -0,0 +1,28 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ * Copyright (C) 2009 Pablo Recio Quijano - pablo.recioquijano@alum.uca.es
+*/
+
+/*Clasico programa en su version C++*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hola, mundo!" << endl;
+  return 0; //No hace falta, pero en fin
+}
---
+++ documentacion/memoria/codigo/hola_mundo.cs
@@ -0,0 +1,15 @@
+//Ejemplo de programa en C#
+
+using System;
+
+namespace HelloNameSpace{
+
+      public class HelloWorld
+
+      {
+            static void Main(string[] args) {
+
+                  Console.WriteLine("Hola Mundo!");
+            }
+      }
+}
